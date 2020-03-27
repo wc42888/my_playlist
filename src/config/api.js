453db218f8fabcd9b26585a1dc9b9c0712b801a7
chao wang <wc42888@hotmail.com>
2020-03-28 09:17:41 +1100

    updated config api file

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,5 @@
 export const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 export const CLIENT_SECRET = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
 export const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
-export const ROOT_URL = 'https://accounts.spotify.com';
+export const ROOT_AUTH_URL = 'https://accounts.spotify.com';
+export const ROOT_URL = 'https://api.spotify.com';
