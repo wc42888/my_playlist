48506d359513f50dcb550015569c85e3b9336f17
chao wang <wc42888@hotmail.com>
2020-03-28 09:22:20 +1100

    added api help functions

---
+++ src/lib/network/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { ROOT_URL } from '../../config/api';
+
+const client = axios.create({
+  baseURL: `${ROOT_URL}/v1`,
+});
+
+const configHeader = (config) => {
+  const accessToken = localStorage.getItem('accessToken');
+  return {
+    ...config,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+};
+
+client.interceptors.request.use((config) => {
+  const newConfig = configHeader(config);
+  return newConfig;
+});
+
+// GET REQUESTS
+export const getUserProfile = () => client.get('/me');
