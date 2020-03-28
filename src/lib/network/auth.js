6a7365b5e55aed162b9de5839a1970e8c6101c15
chao wang <wc42888@hotmail.com>
2020-03-28 18:33:20 +1100

    updated auth with scope

--- src/lib/network/auth.js
+++ src/lib/network/auth.js
@@ -1,8 +1,13 @@
 import moment from 'moment';
-import { CLIENT_ID, REDIRECT_URI, ROOT_AUTH_URL } from '../../config/api';
+import {
+  CLIENT_ID,
+  REDIRECT_URI,
+  ROOT_AUTH_URL,
+  SCOPE,
+} from '../../config/api';
 
 export const getAuth = () =>
-  `${ROOT_AUTH_URL}/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}`;
+  `${ROOT_AUTH_URL}/authorize?client_id=${CLIENT_ID}&response_type=token&scope=${SCOPE}&redirect_uri=${REDIRECT_URI}`;
 
 export const areTokenAndExpiresExist = (token, expires) => token && expires;
 
