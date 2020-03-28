06bce23a1bcdaf3c4c15c59455b1d09ef08d4195
chao wang <wc42888@hotmail.com>
2020-03-28 18:29:45 +1100

    added application scope

--- src/config/api.js
+++ src/config/api.js
@@ -3,3 +3,4 @@ export const CLIENT_SECRET = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
 export const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
 export const ROOT_AUTH_URL = 'https://accounts.spotify.com';
 export const ROOT_URL = 'https://api.spotify.com';
+export const SCOPE = 'playlist-modify-public';
