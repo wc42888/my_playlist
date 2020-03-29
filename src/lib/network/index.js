8b0156aeb792ccb27ce1a0e0b110095097d0d7ea
chao wang <wc42888@hotmail.com>
2020-03-29 14:52:39 +1100

    updated network index.js

--- src/lib/network/index.js
+++ src/lib/network/index.js
@@ -4,6 +4,7 @@ import {
   getUserPlaylists,
   getTracksForPlaylist,
   getSearchResult,
+  postNewTrackToPlaylist,
 } from './api';
 
 export {
@@ -13,4 +14,5 @@ export {
   getUserPlaylists,
   getTracksForPlaylist,
   getSearchResult,
+  postNewTrackToPlaylist,
 };
