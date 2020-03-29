83dfc8781b179676fb1c04caaabdda14788918d1
chao wang <wc42888@hotmail.com>
2020-03-29 14:52:05 +1100

    added tracks actions to post tracks to a play list

--- src/store/actions/tracks.js
+++ src/store/actions/tracks.js
@@ -22,3 +22,32 @@ export const getTracksForPlaylist = (playlistId) => async (dispatch) => {
     dispatch({ type: GET_TRACKS_FOR_PLAYLIST_FAILURE });
   }
 };
+
+export const POST_TRACKS_TO_PLAYLIST_REQUEST =
+  'POST_TRACKS_TO_PLAYLIST_REQUEST';
+
+export const POST_TRACKS_TO_PLAYLIST_SUCCESS =
+  'POST_TRACKS_TO_PLAYLIST_SUCCESS';
+
+export const POST_TRACKS_TO_PLAYLIST_FAILURE =
+  'POST_TRACKS_TO_PLAYLIST_FAILURE';
+
+export const postTracksToPlaylist = (playlistId, tracks) => async (
+  dispatch,
+) => {
+  try {
+    dispatch({ type: POST_TRACKS_TO_PLAYLIST_REQUEST });
+    await api.postNewTrackToPlaylist(playlistId, tracks);
+
+    dispatch({
+      type: POST_TRACKS_TO_PLAYLIST_SUCCESS,
+      payload: {
+        playlistId,
+        tracks,
+      },
+    });
+  } catch (error) {
+    console.log('error', error.message);
+    dispatch({ type: POST_TRACKS_TO_PLAYLIST_FAILURE });
+  }
+};
--- src/store/actions/tracks.test.js
+++ src/store/actions/tracks.test.js
@@ -14,4 +14,18 @@ describe('test tracks actions', () => {
       expect(true).toBeTruthy();
     });
   });
+
+  describe('test post track to a play list function', () => {
+    it(`should dispatch ${actions.POST_TRACKS_TO_PLAYLIST_REQUEST}`, () => {
+      expect(true).toBeTruthy();
+    });
+
+    it(`should dispatch ${actions.POST_TRACKS_TO_PLAYLIST_SUCCESS} on success`, () => {
+      expect(true).toBeTruthy();
+    });
+
+    it(`should displatch ${actions.POST_TRACKS_TO_PLAYLIST_FAILURE} on failure`, () => {
+      expect(true).toBeTruthy();
+    });
+  });
 });
